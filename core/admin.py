af1f0dee91edc1440ace439b0a8af2386e28cbff
Fábio Rolim <fabiorolim@ifpi.edu.br>
2022-09-21 20:50:20 -0300

    Cria os modelos

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Posto, Preco, Combustivel
 
-# Register your models here.
+admin.site.register(Preco)
+admin.site.register(Posto)
+admin.site.register(Combustivel)
