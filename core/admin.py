3636431e4c8c76f4a53d3e5b888639fbdca69f87
Fábio Rolim <fabiorolim@ifpi.edu.br>
2022-09-22 19:54:31 -0300

    Ajusta modelos e restringe visualização de postos no admin

--- core/admin.py
+++ core/admin.py
@@ -3,15 +3,25 @@ from .models import Posto, Preco, Combustivel
 
 
 class PostoAdmin(admin.ModelAdmin):
-    list_display = ('nome_fantasia', 'slug', 'administrador', 'ativo',)
+    list_display = ('nome_fantasia', 'slug', 'adm', 'ativo',)
+    prepopulated_fields = {'slug': ('nome_fantasia',)}
+    readonly_fields = ('adm',)
+
+    def save_model(self, request, obj, form, change):
+        obj.adm = request.user
+        super().save_model(request, obj, form, change)
+
+    def get_queryset(self, request):
+        return Posto.objects.filter(adm=request.user)
 
 
 class CombustivelAdmin(admin.ModelAdmin):
     list_display = ('nome', 'slug', 'ativo')
+    prepopulated_fields = {'slug': ('nome',)}
 
 
 class PrecoAdmin(admin.ModelAdmin):
-    list_display = ('preco', 'posto', 'administrador', 'data_add')
+    list_display = ('preco', 'posto', 'adm', 'data_add')
 
 
 admin.site.register(Preco, PrecoAdmin)
